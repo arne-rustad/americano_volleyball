ae5dee57383d7c1c6d0eae1d6c17ff7b2d447ff1
Arne Rustad <arne.rustad@bearingpoint.com>
2024-08-12 14:39:27 +0200

    Created separate homepage and renamed pages

---
+++ Home.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+# with padel emoji 🏸
+# with team emoji / persons 
+st.header("Americano App for Volleyball🏐")
+
+intro = """\
+This app was created for a volleyball tournament inspired by the padel game \
+Mexicano 🏸 (a variant of Americano), but where more flexibility was needed \
+with respect to number of players on each court and the number of courts in \
+each round. The app allows for a variable number of players on each court and \
+a variable number of courts in each round.
+
+[You can read more about the Mexicano variant here]\
+(https://pistas365.com/padel/information/rules/mexicano-tournament/). \
+The Mexicano padel tournament is a dynamic, social format where players are \
+paired based on their current standings, ensuring evenly matched games as the \
+tournament progresses. Players accumulate individual points from each match, \
+and the player with the most points at the end wins.
+"""
+
+st.markdown(intro)
