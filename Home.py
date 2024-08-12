629d88d9d2e78fc7bb5903c19e985c6909f07754
Arne Rustad <arne.rustad@bearingpoint.com>
2024-08-12 15:13:44 +0200

    Changed description

--- Home.py
+++ Home.py
@@ -3,18 +3,22 @@ import streamlit as st
 st.title("Americano App for Volleyball🏐")
 
 intro = """\
-This app was created for a volleyball tournament inspired by the padel game \
-Mexicano 🏸 (a variant of Americano), but where more flexibility was needed \
+This app was created for a volleyball tournament with a format inspired by the \
+padel game Mexicano 🏸 (a variant of Americano), but where more flexibility was needed \
 with respect to number of players on each court and the number of courts in \
-each round. The app allows for a variable number of players on each court and \
-a variable number of courts in each round.
+each round. The app allows for *a variable number of players on each court* and \
+*a variable number of courts in each round*."""
 
-[You can read more about the Mexicano variant here]\
-(https://pistas365.com/padel/information/rules/mexicano-tournament/). \
+mexicano_description = """\
 The Mexicano padel tournament is a dynamic, social format where players are \
 paired based on their current standings, ensuring evenly matched games as the \
 tournament progresses. Players accumulate individual points from each match, \
-and the player with the most points at the end wins.
+and the player with the most points at the end wins. \
+[You can read more about the Mexicano variant here]\
+(https://pistas365.com/padel/information/rules/mexicano-tournament/). \
 """
 
 st.markdown(intro)
+
+st.subheader("The Mexicano Padel Tournament format")
+st.markdown(mexicano_description)
